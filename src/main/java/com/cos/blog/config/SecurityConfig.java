bcc633a962f1e733d39380aa39fa48a5e54b90af
jangwanjung <tjdrj530@naver.com>
2024-06-04 03:41:08 +0900

    비밀번호 해쉬설정완료

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,21 +1,30 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration //아래코드를 bean등록하는 어노테이션이다
 @EnableWebSecurity //시큐리티 필터를 등록시키는 어노테이션이다
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
 //위에 세개의 어노테이션을 스프링시큐리티를 사용하는데 세트이다
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() //csrf토큰 비활성화(테스트시 걸어두는게좋음)
 			.authorizeHttpRequests() // 어떠한응답이있으면
-				.antMatchers("/auth/**") //auth쪽으로 어떠한 응답이 들어오면
+				.antMatchers("/","/auth/**","/js/**","/css/**","/image/**") //auth쪽으로 어떠한 응답이 들어오면
 				.permitAll() //어떠한보안요구도없이 요청을 허용해준다는 뜻이다
 				.anyRequest() //다른모든요청은
 				.authenticated() // 인증이 되어야 요청을 허용해준다는 뜻이다
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -13,9 +15,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional
 	public void 회원가입(User user) {
-		
+		String rawPassword = user.getPassword();  //원문을받고
+		String encPassword = encoder.encode(rawPassword);  //해쉬를 한다
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 	
